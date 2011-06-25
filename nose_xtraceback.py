c815914252d932e0be154feff7762e89fe6c4641
Arthur Noel <arthur@ischium.net>
2011-06-25 03:45:20 +0100

    fixup

--- nose_xtraceback.py
+++ nose_xtraceback.py
@@ -4,6 +4,8 @@ nose plugin for XTraceback
 This module is placed outside of the xtraceback package because we don't want
 nose to import the xtraceback package until it has started the coverage plugin.
 """
+import sys
+
 try:
     from nose.plugins import Plugin
 except ImportError:
@@ -14,24 +16,37 @@ else:
         """Extended traceback for error output"""
         
         name = "xtraceback"
-            
+        
+        _options = (
+            ("color", "Show color tracebacks [%s]", "store_true"),
+            ("globals", "Include globals in tracebacks [%s]", "store_true"),
+            ("globals_include", "Include only globals in this namespace [%s]", "store")
+            )
+        
         def options(self, parser, env):
             super(nose_xtraceback, self).options(parser, env)
-            env_opt = "NOSE_XTRACEBACK_GLOBALS"
-            parser.add_option("--xtraceback-globals",
-                              action="store_true",
-                              dest="show_globals",
-                              default=env.get(env_opt),
-                              help="Include globals in tracebacks [%s]" % env_opt)
+            for name, help, action in self._options:
+                env_opt = "NOSE_XTRACEBACK_%s" % name.upper()
+                parser.add_option("--xtraceback-%s" % name.replace("_", "-"),
+                                  action=action,
+                                  dest="xtraceback_%s" % name,
+                                  default=env.get(env_opt),
+                                  help=help % env_opt)
+            
         
         def configure(self, options, conf):
             super(nose_xtraceback, self).configure(options, conf)
-            self.show_globals = options.show_globals
+            for name, help, action in self._options:
+                name = "xtraceback_%s" % name
+                setattr(self, name, getattr(options, name))
                 
         def begin(self):
-            from xtraceback import activate
-            activate(show_globals=self.show_globals)
-        
+            from xtraceback import TracebackCompat
+            options = dict(show_globals=self.xtraceback_globals,
+                           globals_module_include=self.xtraceback_globals_include,
+                           color=self.xtraceback_color,)
+            self._xtraceback_compat = TracebackCompat(**options)
+            self._xtraceback_compat.__enter__()
+            
         def finalize(self, result):
-            from xtraceback import deactivate
-            deactivate()
+            self._xtraceback_compat.__exit__(None, None, None)
