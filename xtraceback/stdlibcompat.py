f5d8ffa018b7f9cc12f741f865b7be2f0aa37d67
Arthur Noel <arthur@ischium.net>
2011-12-01 12:37:58 +0000

    working loggging.Formatter patch

--- xtraceback/stdlibcompat.py
+++ xtraceback/stdlibcompat.py
@@ -49,19 +49,19 @@ class StdlibCompat(object):
 
     def install_logformatter(self, formatter, **options):
 
-        myself = self
-        self._patch(formatter,
-                    "formatException",
-                    lambda self, ei: myself.format_exception(*ei, **options))
+        def formatException(ei):
+            xtb = self._factory(*ei, **options)
+            return str(xtb)
+        self._patch(formatter, "formatException", formatException)
 
         # this is shit but we're stuck with the stdlib implementation since
         # it caches the result of formatException which we don't want to do
         # as it will screw up other formatters who are expecting a regular
         # traceback
         _format = formatter.format
-        def format(self, record):
+        def format(record):
             record.exc_text = None
-            formatted = _format(self, record)
+            formatted = _format(record)
             record.exc_text = None
             return formatted
         self._patch(formatter, "format", format)
