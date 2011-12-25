65fa5f955544a268034b3d2e2b2d639cf904f834
Arthur Noel <arthur@ischium.net>
2011-12-25 11:13:25 +0000

    diff right way around

--- xtraceback/test/cases.py
+++ xtraceback/test/cases.py
@@ -44,8 +44,8 @@ class XTracebackTestCase(TestCaseMixin, unittest.TestCase):
         # line
         exc_str = TRAILING_WHITESPACE_PATTERN.sub("\n", exc_str)
         if exc_str != expect_exc_str:  # pragma: no cover for obvious reasons
-            diff = difflib.ndiff(exc_str.splitlines(True),
-                                 expect_exc_str.splitlines(True))
+            diff = difflib.ndiff(expect_exc_str.splitlines(True),
+                                 exc_str.splitlines(True))
             #print "-" * 70
             #print "want:"
             #print expect_exc_str
