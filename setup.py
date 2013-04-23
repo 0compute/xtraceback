005a7b1f8b1df5d74b0175f1683aceeb41828556
Arthur Noel <arthur@0compute.net>
2013-04-23 16:27:52 +0100

    doc

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ import os
 
 from setuptools import setup
 
-# under test we do not want the nose entry point
+# under test we do not want the nose entry point installed because it screws up
+# coverage as the xtraceback module is imported too early
 if "XTRACEBACK_NO_NOSE" in os.environ:
     entry_points = None
 else:
     entry_points = {
         "nose.plugins": ("xtraceback=xtraceback.nosextraceback:NoseXTraceback",),
-        }
+    }
 
 README = open(os.path.join(os.path.dirname(__file__), "README.md"))
 
